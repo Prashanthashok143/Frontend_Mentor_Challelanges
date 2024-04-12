cf1bd5889fa88b204e1c6d4c00d01e0f005b3307
Prashanth <Prashanthashok143@gmail.com>
2024-04-12 16:05:40 +0530

    added new folder

---
+++ faq-accordion-main/faq-accordion-main/jsfile.js
@@ -0,0 +1,75 @@
+
+
+let isNameChanged1 = false;
+function changeButton1() {
+    
+    const button1= document.getElementById("myButton1");
+    const FirstAns=document.getElementById("FirstAns")
+     FirstAns.classList.toggle("More1")
+    
+
+    if (isNameChanged1) {
+        button1.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg>`;
+    } else {
+        button1.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#301534" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"/></svg>`;
+    }
+    isNameChanged1 = !isNameChanged1; 
+}
+
+
+
+
+
+let isNameChanged2 = false;
+function changeButton2() {
+    
+    const button2 = document.getElementById("myButton2");
+    const SecondAns=document.getElementById("SecondAns")
+     SecondAns.classList.toggle("More2")
+    
+
+    if (isNameChanged2) {
+        button2.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg>`;
+    } else {
+        button2.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#301534" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"/></svg>`;
+    }
+    isNameChanged2 = !isNameChanged2; 
+}
+
+
+
+
+let isNameChanged3 = false;
+function changeButton3() {
+    
+    const button3 = document.getElementById("myButton3");
+    const ThirdAns=document.getElementById("ThirdAns")
+     ThirdAns.classList.toggle("More3")
+    
+
+    if (isNameChanged3) {
+        button3.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg>`;
+    } else {
+        button3.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#301534" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"/></svg>`;
+    }
+    isNameChanged3 = !isNameChanged3; 
+}
+
+
+
+
+let isNameChanged4 = false;
+function changeButton4() {
+    
+    const button4 = document.getElementById("myButton4");
+    const FourthAns=document.getElementById("FourthAns")
+     FourthAns.classList.toggle("More4")
+    
+
+    if (isNameChanged4) {
+        button4.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#AD28EB" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.203 12.203 0 0 0 15 3.312Zm4.688 13.124h-3.75v3.75a.938.938 0 0 1-1.876 0v-3.75h-3.75a.938.938 0 0 1 0-1.875h3.75v-3.75a.938.938 0 0 1 1.876 0v3.75h3.75a.938.938 0 0 1 0 1.876Z"/></svg>`;
+    } else {
+        button4.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="30" height="31" fill="none" viewBox="0 0 30 31"><path fill="#301534" d="M15 3.313A12.187 12.187 0 1 0 27.188 15.5 12.2 12.2 0 0 0 15 3.312Zm4.688 13.124h-9.375a.938.938 0 0 1 0-1.875h9.374a.938.938 0 0 1 0 1.876Z"/></svg>`;
+    }
+    isNameChanged4 = !isNameChanged4; 
+}
